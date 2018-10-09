f4883a1b3aac9a2566be0c3d53790b4fa5af0b6c
Robert Schulte <schulte.robertw@outlook.com>
2018-10-09 17:21:37 -0500

    added loop to handle invalid characters

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,40 +2,63 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-  # Find character_hash that matches character given input array_of_characters
-
-def get_character_movies_from_api(character)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  
-  # Finding the array of all characters given response_hash
-  array_of_characters = response_hash["results"]
-
-  character_hash = find_character(character, array_of_characters)
-  films_array = character_hash["films"]
-
-  #binding.pry
-
-  new_films_array = films_array.map do |movie_url|
-    response_string = RestClient.get("#{movie_url}")
-    response_hash = JSON.parse(response_string)
-  end 
-
-  #binding.pry 
+# Make web request to an API, store JSON in hash variable
+def web_request(url)
+  JSON.parse(RestClient.get("#{url}"))
 end
 
+# Pull value from hash given key
+def get_hash_value(hash, key)
+  hash[key]
+end
+
+# Finds desired character given an array of character hashes
 def find_character(character, array_of_characters)
   array_of_characters.each do |character_hash|
     if character_hash["name"].downcase == character.downcase
       return character_hash
     end
-    # loop back to the welcome message
   end
-puts "Character not found."
+  return {}
 end 
 
-#get_character_movies_from_api("C-3PO")
+# prints error message if character does not exist in API
+def invalid_character
+  puts "Character not found. Try another."
+  return false
+end
+
+# iterate over an array of URL's and makes web requests for each
+def url_mapper(url_array)
+  url_array.map do |url|
+    web_request(url)
+  end
+end
+
+# Takes character as input, returns array of film hashes
+def get_character_movies_from_api(character)
+
+  # Make web request to Star Wars API, store resulting hash in variable
+  response_hash = web_request('http://www.swapi.co/api/people/')
+
+  # Finding the array of all characters given response_hash
+  array_of_characters = get_hash_value(response_hash, "results")
+
+  # Pull desired character hash from character array
+  character_hash = find_character(character, array_of_characters)
+  
+  # Check that character exists
+  if character_hash == {}
+    return nil
+  end
+
+  # Store films for a given character in an array (of hashes)
+  films_array = get_hash_value(character_hash, "films")
+
+  # Iterate over films, making web requests for each, store film data in new array
+  new_films_array = url_mapper(films_array)
+  
+end
 
 def print_movies(new_films_array)
   new_films_array.each do |film_hash|
@@ -45,10 +68,11 @@ end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
+
+  # Check if film array exists (it won't if character was invalid)
+  if films_array == nil
+    invalid_character
+  else
   print_movies(films_array)
+  end
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,6 +4,6 @@ end
 
 def get_character_from_user
   puts "please enter a character"
-  gets.chomp.downcase
+  gets.chomp
 end
 
