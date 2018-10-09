214124df311d818e95e6947d99d552a3c9087d73
Robert Schulte <schulte.robertw@outlook.com>
2018-10-09 15:45:15 -0500

    wrote communicator.rb file

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,28 +2,45 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+  # Find character_hash that matches character given input array_of_characters
+
 def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
   
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
+  # Finding the array of all characters given response_hash
+  array_of_characters = response_hash["results"]
 
+  character_hash = find_character(character, array_of_characters)
+  films_array = character_hash["films"]
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  #binding.pry
+
+  new_films_array = films_array.map do |movie_url|
+    response_string = RestClient.get("#{movie_url}")
+    response_hash = JSON.parse(response_string)
+  end 
+
+  #binding.pry 
 end
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def find_character(character, array_of_characters)
+  array_of_characters.each do |character_hash|
+    if character_hash["name"].downcase == character.downcase
+      return character_hash
+    end
+    # loop back to the welcome message
+  end
+puts "Character not found."
+end 
+
+#get_character_movies_from_api("C-3PO")
+
+def print_movies(new_films_array)
+  new_films_array.each do |film_hash|
+    puts film_hash["title"]
+  end
 end
 
 def show_character_movies(character)
